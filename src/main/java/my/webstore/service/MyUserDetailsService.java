88888f34daf12f827f13f080f41c04f64e72f2d4
Kacper Kulka <kuleczkomen@gmail.com>
2026-02-21 20:30:07 +0100

    corrected addUser - now users must have different emails

--- src/main/java/my/webstore/service/MyUserDetailsService.java
+++ src/main/java/my/webstore/service/MyUserDetailsService.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
@@ -17,14 +19,14 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repo.findByEmail(email);
+        Optional<User> user = repo.findByEmail(email);
 
-        if(user == null) {
-            IO.println("User with email '%s' not found".formatted(email));
+        if(user.isEmpty()) {
+            // IO.println("User with email '%s' not found".formatted(email));
             throw new UsernameNotFoundException("user not found");
         }
 
-        return new UserPrincipal(user);
+        return new UserPrincipal(user.get());
 
      }
 }
--- src/main/java/my/webstore/service/UserService.java
+++ src/main/java/my/webstore/service/UserService.java
@@ -3,7 +3,9 @@ package my.webstore.service;
 import lombok.RequiredArgsConstructor;
 import my.webstore.model.User;
 import my.webstore.repo.UserRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,12 +20,15 @@ public class UserService {
     }
 
     public void addUser(User user) {
-        IO.println(repo.findByEmail(user.getEmail()));
-        if(repo.findByEmail(user.getEmail()) != null) {
-            IO.println("Email already exists");
-        } else {
-            repo.save(user);
-        }
+        // not allowing users with the same email
+        repo.findByEmail(user.getEmail()).ifPresent(u -> {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Email already in database"
+            );
+        });
+
+        repo.save(user);
     }
 
 
